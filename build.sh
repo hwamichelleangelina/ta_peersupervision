ce35c55eb3c83793dc6d600937a4cb49a823c4f6
18320007 Michelle Angelina <hwamichelleangelina@gmail.com>
2024-06-19 11:27:28 +0700

    Deployment Progress 19-06-2024 11:25

--- build.sh
+++ build.sh
@@ -9,18 +9,21 @@ pwd
 ls -la
 
 # Install Flutter
-FLUTTER_VERSION="3.7.0"
+FLUTTER_VERSION="3.10.5"
+DART_VERSION="3.3.2"
 wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Check Flutter version
-flutter --version
+# Verify Flutter installation
+flutter doctor
+
+# Ensure Dart SDK is the correct version
+flutter doctor --version
 
 # Enable web support
 flutter config --enable-web
 
 # Build Web
-flutter doctor
 flutter pub get
 flutter build web --release --no-tree-shake-icons
